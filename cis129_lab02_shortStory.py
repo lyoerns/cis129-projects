287f7be20ff6a3b6b9a9bb12808d60fced856904
lyoerns <lyoerns@mail.pima.edu>
2024-09-09 19:00:06 -0700

    Update cis129_lab02_shortStory.py
    - added print commands to make a short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+# Author: Lee Yoerns
+# CIS129 lab
+# Prints a short story about an endless time loop
+print('An intruder sits in my chair; he wears my face.')
+print('I kill him and take back my rightful place.')
+print('Another man walks through the door.')
+print('I sit in his chair and wear his face.')
+print('I know what is about to happen next.')
+
