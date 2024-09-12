6407847e2dbc17060585f5e90ac40107145d8c7a
lyoerns <lyoerns@mail.pima.edu>
2024-09-12 11:45:35 -0700

    Update and rename Mod3Lab.py to cis129_lab03_coffeeShop.py
    - added documentation 
    - added additional comments regarding round command

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,7 +1,8 @@
-# Write program that asks user for number of coffee and muffins they purchase
+#cis129_lab03_coffeeShop.py
+"""Write a program that asks user for number of coffee and muffins they purchase
+  and calculate the total price plus tax."""
 # Price of coffee is $5 and price of muffin is $4. Calculate 6% tax on subtotal. 
 ###
-
 # objects: coffee_price and muffin_price (constant)
 # user input: coffee_num and muffin_num
 ## coffee_total = coffee_num * coffee_price
@@ -18,5 +19,8 @@ coffee_total = coffee_num * coffee_price
 muffin_total = muffin_num * muffin_price
 subtotal = coffee_total + muffin_total
 final_calc = subtotal * 1.06
+# additional variable final_calc was created in order to use round command
+# it didn't work when final_cost was rounded on its own
+# python was adding multiple decimal places to the printed final_cost
 final_cost = round(final_calc, 2)
 print('Total: $', final_cost)
